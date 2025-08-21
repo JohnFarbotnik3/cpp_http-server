0ee991b4efc3fcc31b5d98fb2980d1800fe8bb43
john <>
2025-08-20 20:36:40 -0400

    static_file_server module refactors

--- src/handlers/static_file_server.cpp
+++ src/handlers/static_file_server.cpp
@@ -1,20 +1,29 @@
+#include <cstdint>
 #include <cstdlib>
 #include <filesystem>
+#include <shared_mutex>
 #include <string>
 #include "../definitions/methods.cpp"
 #include "../definitions/headers.cpp"
 #include "../definitions/mime_types.cpp"
 #include "../HTTPServer.cpp"
-#include "../utils/file_io.cpp"
 #include "src/utils/config_util.cpp"
 #include "src/http_util.cpp"
 
-// TODO - add shared map with paths of locked files to prevent read/write collisions.
+// TODO - review for thread safety.
+/*
+https://man7.org/linux/man-pages/man2/pwrite.2.html
+NOTE: pread/pwrite allows multiple threads to use the same file-descriptor
+
+https://man7.org/linux/man-pages/man2/read.2.html
+https://man7.org/linux/man-pages/man2/open.2.html
+https://man7.org/linux/man-pages/man2/close.2.html
+https://stackoverflow.com/questions/73601293/thread-safe-file-updates
+*/
 namespace HTTP::Handlers::static_file_server {
 	using std::string;
-	namespace fio = utils::file_io;
 	namespace fs = std::filesystem;
-	using fs::path;
+
 
 	struct static_file_server_config {
 		string prefix = "";// prefix to append to target filepaths.
@@ -32,9 +41,11 @@ namespace HTTP::Handlers::static_file_server {
 		}
 	};
 
+
 	struct static_file_server_struct {
 		static_file_server_config config;
-		path prefix_canonical;
+		fs::path prefix_canonical;
+		SharedMap<string, std::shared_mutex> file_access_map;
 
 		static_file_server_struct(static_file_server_config config) {
 			this->config = config;
@@ -54,20 +65,93 @@ namespace HTTP::Handlers::static_file_server {
 		void return_status(http_response& response, const int status_code) {
 			response.status_code = status_code;
 		}
-		size_t append_file(MessageBuffer& bodybuf, const path target) {
-			std::ifstream file(target, std::ios::binary | std::ios::ate);
-			if (!file.is_open()) {
-				fprintf(stderr, "failed to open file for reading: %s\n", target.c_str());
-				fprintf(stderr, "errno: %s\n", strerror(errno));
-				return -1;
+
+		void handle_GET(const http_request& request, http_response& response, MessageBuffer& body_buffer, const fs::path path, const bool do_get) {
+			if(!config.can_get_files) return return_status(response, 403);
+			bool can_read_file = fs::exists(path) && fs::is_regular_file(path);
+			if(!can_read_file) return return_status(response, 404);
+
+			std::error_code ec;
+			uintmax_t file_size = fs::file_size(path, ec);
+			if(ec) {
+				fprintf(stderr, "[handle_GET] failed to get file-size: %s [%s]\n", ec.message().c_str(), path.c_str());
+				return_status(response, 500);
+			}
+
+			// TODO - range request support.
+			size_t offset = 0;
+			size_t count = file_size;// TODO - limit max count to prevent memory allocation DOS.
+			if(offset > file_size) offset = file_size;
+			if(count > file_size) count = std::min(count, file_size - offset);
+
+			ssize_t n_read;
+			if(do_get) {
+				int fd = open(path.c_str(), O_RDONLY);
+				if(fd == -1) {
+					fprintf(stderr, "[handle_GET] failed to open file: %s [%s]\n", strerror(errno), path.c_str());
+					return_status(response, 404);
+				}
+
+				body_buffer.reserve(count);
+				n_read = pread(fd, body_buffer.data, count, offset);
+				close(fd);
+
+				if(n_read == -1) {
+					fprintf(stderr, "[handle_GET] failed to read file: %s [%s]\n", strerror(errno), path.c_str());
+					return_status(response, 500);
+				} else {
+					body_buffer.length = n_read;
+				}
 			} else {
-				const size_t len = file.tellg();
-				bodybuf.reserve(bodybuf.length + len);
-				file.seekg(0);
-				file.read(bodybuf.data + bodybuf.length, len);
-				file.close();
-				bodybuf.length += len;
-				return len;
+				n_read = count;
+			}
+
+			response.headers[HTTP::HEADERS::content_type] = get_mime_type(path.extension());
+			response.headers[HTTP::HEADERS::content_length] = int_to_string(n_read);
+			return return_status(response, 200);
+		}
+
+		void handle_PUT(const http_request& request, http_response& response, MessageBuffer& body_buffer, const fs::path path) {
+			if(!config.can_put_files) return return_status(response, 403);
+			const bool can_write_file = fs::is_directory(path.parent_path()) && (!fs::exists(path) || fs::is_regular_file(path));
+			if(!can_write_file) return return_status(response, 405);
+
+			int status;
+			bool file_exists = fs::is_regular_file(path);
+
+			// TODO - range request support.
+			size_t offset = 0;// TODO - limit offset to prevent sparse-file allocation DOS.
+			size_t count = request.body.size();
+
+			int fd = open(path.c_str(), O_WRONLY | O_CREAT);
+			if(fd == -1) {
+				fprintf(stderr, "[handle_PUT] failed to open file: %s [%s]\n", strerror(errno), path.c_str());
+				return_status(response, 404);
+			}
+
+			ssize_t n_write = pwrite(fd, body_buffer.data, count, offset);
+			close(fd);
+
+			if(n_write == -1) {
+				fprintf(stderr, "[handle_PUT] failed to write file: %s [%s]\n", strerror(errno), path.c_str());
+				return_status(response, 500);
+			}
+
+			return return_status(response, file_exists ? 204 : 201);
+		}
+
+		void handle_DELETE(const http_request& request, http_response& response, MessageBuffer& body_buffer, const fs::path path) {
+			if(!config.can_delete_files) return return_status(response, 403);
+			const bool can_delete_file = fs::exists(path) && fs::is_regular_file(path);
+			if(!can_delete_file) return return_status(response, 404);
+
+			std::error_code ec;
+			bool success = fs::remove(path, ec);
+			if(!success) {
+				fprintf(stderr, "[handle_DELETE] failed to delete file: %s [%s]\n", ec.message().c_str(), path.c_str());
+				return return_status(response, 500);
+			} else {
+				return return_status(response, 204);
 			}
 		}
 
@@ -79,18 +163,18 @@ namespace HTTP::Handlers::static_file_server {
 			response.headers[HTTP::HEADERS::content_length] = "0";
 
 			// modify target file path.
-			path target;
+			fs::path target;
 			if(request.path == "/") {
-				target = path(prefix_canonical / "index.html");
+				target = fs::path(prefix_canonical / "index.html");
 			} else {
 				string str = request.path;
 				if(str.starts_with("/")) str = str.substr(1);
-				target = path(prefix_canonical / str);
+				target = fs::path(prefix_canonical / str);
 			}
 
 			// SECURITY: ensure target file is both valid and within prefix directory.
 			std::error_code ec;
-			path target_canonical = fs::weakly_canonical(target, ec);
+			fs::path target_canonical = fs::weakly_canonical(target, ec);
 			if(ec) {
 				fprintf(stderr, "%s\n", ec.message().c_str());
 				return return_status(response, 500);
@@ -104,52 +188,11 @@ namespace HTTP::Handlers::static_file_server {
 			}
 			target = target_canonical;
 
-			// ============================================================
 			// request methods.
-			// ------------------------------------------------------------
-
-			const bool method_get = request.method == HTTP::METHODS::GET;
-			const bool method_head = request.method == HTTP::METHODS::HEAD;
-			if(method_get || method_head) {
-				if(!config.can_get_files) return return_status(response, 403);
-				bool can_read_file = fs::exists(target) && fs::is_regular_file(target);
-				if(!can_read_file) return return_status(response, 404);
-
-				size_t len;
-				if(method_get) {
-					len = append_file(body_buffer, target);
-					if(len < 0) return_status(response, 500);
-				} else {
-					std::error_code ec;
-					len = fs::file_size(target, ec);
-					if(ec) return_status(response, 500);
-				}
-				response.headers[HTTP::HEADERS::content_type] = get_mime_type(target.extension());
-				response.headers[HTTP::HEADERS::content_length] = int_to_string(len);
-				return return_status(response, 200);
-			}
-
-			if(request.method == HTTP::METHODS::PUT) {
-				if(!config.can_put_files) return return_status(response, 403);
-				const bool can_write_file = fs::is_directory(target.parent_path()) && (!fs::exists(target) || fs::is_regular_file(target));
-				if(!can_write_file) return return_status(response, 405);
-
-				int status;
-				bool file_exists = fs::is_regular_file(target);
-				fio::write_file(target, status, request.body.data(), request.body.size());
-				return return_status(response, (status != 0) ? 500 : (file_exists ? 204 : 201));
-			}
-
-			if(request.method == HTTP::METHODS::DELETE) {
-				if(!config.can_delete_files) return return_status(response, 403);
-				const bool can_delete_file = fs::exists(target) && fs::is_regular_file(target);
-				if(!can_delete_file) return return_status(response, 404);
-
-				int status;
-				fio::delete_file(target, status);
-				return return_status(response, (status != 0) ? 500 : 204);
-			}
-
+			if(request.method == HTTP::METHODS::GET		) return handle_GET(request, response, body_buffer, target, true);
+			if(request.method == HTTP::METHODS::HEAD	) return handle_GET(request, response, body_buffer, target, false);
+			if(request.method == HTTP::METHODS::PUT		) return handle_PUT(request, response, body_buffer, target);
+			if(request.method == HTTP::METHODS::DELETE	) return handle_DELETE(request, response, body_buffer, target);
 			return return_status(response, 501);
 		}
 
